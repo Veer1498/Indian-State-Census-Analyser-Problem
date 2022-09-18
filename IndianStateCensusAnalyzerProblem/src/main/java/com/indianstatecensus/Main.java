87aa749539c19de93adccded74debdf4adfa5f18
Veer1498 <singavarapuveerendra@gmail.com>
2022-09-19 00:15:04 +0530

    [Veer] Added : Welcome Message

---
+++ IndianStateCensusAnalyzerProblem/src/main/java/com/indianstatecensus/Main.java
@@ -0,0 +1,7 @@
+package com.indianstatecensus;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian State Census Analyzer Program");
+    }
+}
